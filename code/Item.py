9dacbb9440c8d4e76bda57f15b2b8d7767c1cc9d
Grazielle Vitoria <GraziVitt@gmail.com>
2026-06-29 02:08:56 -0300

    Penultima

--- code/Item.py
+++ code/Item.py
@@ -3,20 +3,24 @@ import pygame
 
 class Item:
 
-    def __init__(self, name, image_path, position):
+    def __init__(self, name, image_path, position, size=(32, 32)):
 
         self.name = name
 
         self.image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (32, 32))
+
+        self.image = pygame.transform.scale(
+            self.image,
+            size
+        )
 
         self.start_position = position
 
         self.x = position[0]
         self.y = position[1]
 
-        self.width = 32
-        self.height = 32
+        self.width = size[0]
+        self.height = size[1]
 
         self.collected = False
 
@@ -27,6 +31,54 @@ class Item:
 
     def get_rect(self):
 
+        if self.name == "cafe":
+            return pygame.Rect(
+                self.x - 15,
+                self.y - 40,
+                self.width + 30,
+                self.height + 80
+            )
+
+        elif self.name == "documentos":
+            return pygame.Rect(
+                self.x - 10,
+                self.y - 20,
+                self.width + 0,
+                self.height + 70
+            )
+
+        elif self.name == "caneta":
+            return pygame.Rect(
+                self.x - 20,
+                self.y - 20,
+                self.width + 40,
+                self.height + 40
+            )
+
+        elif self.name == "copo":
+            return pygame.Rect(
+                self.x - 20,
+                self.y - 30,
+                self.width + 40,
+                self.height + 60
+            )
+
+        elif self.name == "grampeador":
+            return pygame.Rect(
+                self.x - 20,
+                self.y - 20,
+                self.width + 60,
+                self.height + 60
+            )
+
+        elif self.name == "pasta":
+            return pygame.Rect(
+                self.x - 20,
+                self.y - 25,
+                self.width + 40,
+                self.height + 50
+            )
+
         return pygame.Rect(
             self.x,
             self.y,
@@ -35,9 +87,9 @@ class Item:
         )
 
     def reset(self):
-        """Volta o item para o lugar de origem."""
 
         self.collected = False
 
         self.x = self.start_position[0]
         self.y = self.start_position[1]
+
